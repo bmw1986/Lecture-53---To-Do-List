9a7ba1d255ee66123671c991c7934e8f3ac6afd8
Freddy Oakes <freddy.oakes@me.com>
2015-03-16 17:53:31 -0400

    Initial Commit

---
+++ Lecture 53 - To Do List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Lecture 53 - To Do List
+//
+//  Created by Freddy Oakes on 3/16/15.
+//  Copyright (c) 2015 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

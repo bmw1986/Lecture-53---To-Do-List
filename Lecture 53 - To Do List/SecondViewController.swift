99cec325123e17b4ab2d34cee4a64761ce547332
Freddy <freddy.oakes@me.com>
2015-03-28 12:22:24 -0400

    Added TableView logic.

--- Lecture 53 - To Do List/SecondViewController.swift
+++ Lecture 53 - To Do List/SecondViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var item: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text)
+        
+        item.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,16 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        item.resignFirstResponder()
+        return true
+    }
+    
 }
 
